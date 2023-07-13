f34cc94b577b2dce2862922053d09aba2817c301
Bogdan Savchenko <magadan_93@mail.ru>
2023-07-13 17:23:06 +0700

    JavaBase HW1, реализовал методы save, get, delete, clear, getAll, size в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,51 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        storage[getNextIndex()] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume!= null && uuid.equals(resume.uuid))
+                return resume;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        boolean deletedFlag = false;
+        for (int i = 0; i < storage.length; i++) {
+            if(deletedFlag) {
+                storage[i-1] = storage[i];
+            } else if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                deletedFlag = true;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[getNextIndex()];
+        System.arraycopy(storage, 0, resumes, 0, resumes.length);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return getNextIndex();
+    }
+
+    private int getNextIndex() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null)
+                return i;
+        }
+        return storage.length-1;
     }
 }
